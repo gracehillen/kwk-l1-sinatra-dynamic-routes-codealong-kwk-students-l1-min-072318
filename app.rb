04ddd845cfc15a8b02b187694e947c21dce0f2da
gracehillen <grahillen19@ahastars.org>
2018-07-31 18:55:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,9 +22,9 @@ class App < Sinatra::Base
 #   :id => 1
 # }
   get '/mutiply/:number1/:number2' do
-    @number1 = params[:number1].to_i 
-    @number2 = params[:number2].to_i 
-    (@number1 * @number2).to_s 
+    @num1 = params[:number1].to_i 
+    @num2 = params[:number2].to_i 
+    (@num1 * @num2).to_s 
   end
 end
 
